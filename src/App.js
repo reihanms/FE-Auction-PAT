c9810af02a91921de6107a74f68c2d8fecc2138d
sndff <sandifaisal41@gmail.com>
2023-11-28 13:45:38 +0700

    Merge with alip's

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Landingpage from './Pages/Landingpage'
 import Loginpage from './Pages/Loginpage'
+import Home from './Component/Home'
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landingpage/>}/>
         <Route path="/login" element={<Loginpage/>}/>
+        <Route path="/home" element={<Home/>}/>
       </Routes>
     </Router>
   )
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomePage from '../Component/Home'
+
+export const Home = () => {
+  return <HomePage/>
+}
+
+export default Home
