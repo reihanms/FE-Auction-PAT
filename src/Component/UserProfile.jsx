5ab2b0e56103060fe7ab76883ed39cff1d181c0e
reihanms <reihanmanzis1@gmail.com>
2023-12-19 14:58:52 +0700

    feat: update profile

--- src/Component/UserProfile.jsx
+++ src/Component/UserProfile.jsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer } from "./Footer";
 import { useAuth } from "../helpers/hooks/Authentication";
+import { useFormik } from "formik";
+import { postUploadFile, putProfile } from "../helpers/actions/api";
 // import defaultProfile from "../../public/assets/profiledefault.jpg";
+import Swal from "sweetalert2";
+
 export const UserProfile = () => {
-  const { user } = useAuth();
+  const [isEdit, setIsEdit] = useState(false);
+  const { user, init } = useAuth();
+  const initialData = {
+    profile_picture: user?.profile_picture,
+    name: user?.name,
+    email: user?.email,
+    address: user?.address,
+    about: user?.about,
+  };
+  const formik = useFormik({
+    initialValues: initialData,
+    enableReinitialize: true,
+    onSubmit: (values, { props, resetForm, setErrors, setSubmitting }) =>
+      // console.log(values)
+      onEditProfile(values, resetForm, setErrors, setSubmitting),
+  });
+
+  const onEditProfile = async (values, resetForm, setErrors, setSubmitting) => {
+    await putProfile(values)
+      .then((response) => {
+        console.log(response);
+        const { code, message } = response;
+        if (code === 200) {
+          setIsEdit(false);
+          formik.resetForm();
+          init();
+          Swal.fire({
+            icon: "success",
+            title: "Edit Success!",
+            text: "",
+            confirmButtonText: "OK",
+          });
+        } else {
+          setIsEdit(false);
+          formik.resetForm();
+          Swal.fire({
+            icon: "error",
+            title: "Gagal",
+            text: message,
+            confirmButtonText: "Kembali",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsEdit(false);
+        formik.resetForm();
+      })
+      .finally(() => setSubmitting(false));
+  };
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file, file.name);
+    const res = await postUploadFile(formData);
+    formik.setFieldValue("profile_picture", res.data.url);
+  };
   return (
     <>
       <div class="profile-container">
@@ -23,17 +83,57 @@ export const UserProfile = () => {
           className="profile-icon-home"
         />
         <div class="profile-username">
-          <h2>{user?.username}</h2>
-          <p>{user?.address}</p>
+          {!isEdit ? (
+            <div>
+              <h2>{user?.name}</h2>
+              <p>{user?.address}</p>
+              <p>{user?.email}</p>
+            </div>
+          ) : (
+            <div>
+              <h2>
+                <input
+                  type="text"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  name="name"
+                />
+              </h2>
+              <p>
+                <input
+                  type="text"
+                  value={formik.values.address}
+                  onChange={formik.handleChange}
+                  name="address"
+                />
+              </p>
+              <p>
+                <input
+                  type="text"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  name="email"
+                />
+              </p>
+            </div>
+          )}
         </div>
       </div>
       <div class="card-container">
         <div class="card">
           <img
-            src={user?.picture ? user.picture : "/assets/profiledefault.jpg"}
+            src={user?.profile_picture ? user.profile_picture : "/assets/profiledefault.jpg"}
             alt="CardImage"
             class="card-image"
           />
+          {isEdit && (
+            <input
+              type="file"
+              className="in"
+              style={{ width: "100%", maxWidth: "500px" }}
+              onChange={(e) => handleFileChange(e)}
+            />
+          )}
           <div class="card-content">
             <table className="profile-table">
               <tr>
@@ -55,13 +155,23 @@ export const UserProfile = () => {
 
       <div className="profile-deskripsi-container">
         <h2>About me</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          non fugiat quo commodi molestiae tempora corporis ad, aliquid eum
-          magnam iusto maxime labore magni laborum beatae corrupti illo nisi
-          sed?
-        </p>
+        {!isEdit ? (
+          <p>{user?.about}</p>
+        ) : (
+          <input
+            type="text"
+            value={formik.values.about}
+            onChange={formik.handleChange}
+            name="about"
+          />
+        )}
       </div>
+      {isEdit && (
+        <button onClick={formik.handleSubmit} type="submit">
+          Save Edit
+        </button>
+      )}
+      <button onClick={() => setIsEdit(!isEdit)}>Edit Profile</button>
 
       <div className="profile-button">
         <div className="col-md-4">
